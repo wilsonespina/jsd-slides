e446ccb47a2dc1f96b0908155b4eeb6820fbfee3
Wilson Espina <wilson.espina@gmail.com>
2020-07-01 14:49:37 +0100

    Update lesson 15

--- dist/src/week-01/lesson-01/index.js
+++ dist/src/week-01/lesson-01/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50687" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53944" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
