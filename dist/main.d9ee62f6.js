891e9c787fc074ce2e5833c2acf25787503ddbb4
Wilson Espina <wilson.espina@gmail.com>
2020-07-04 16:59:31 +0100

    Add read functionality slides.

--- dist/main.d9ee62f6.js
+++ dist/main.d9ee62f6.js
@@ -227,7 +227,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60711" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56862" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
